ddd19aa9f78d71d98aa2944bd1e35b13de5ccddb
omerakcali <omerakcali99@gmail.com>
2024-08-06 01:31:06 +0300

    tnt particle

--- Assets/Scripts/BoardElements/TNT.cs
+++ Assets/Scripts/BoardElements/TNT.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using Unity.Mathematics;
 using UnityEngine;
 
 public class TNT : BoardElement
 {
+    [SerializeField] private GameObject ParticleGO;
     [SerializeField] private Vector2Int ExplosionSize;
     public override BoardElementType ElementType => BoardElementType.TNT;
     public override bool DoesFall => true;
@@ -25,6 +27,7 @@ public class TNT : BoardElement
 
     public override void OnSpawn()
     {
+        ParticleGO.SetActive(false);
         _popped = false;
     }
 
@@ -34,20 +37,25 @@ public class TNT : BoardElement
         {
             return;
         }
+        ParticleGO.SetActive(true);
         _popped = true;
-        for (int i = -ExplosionSize.x; i <= ExplosionSize.x; i++)
+        DOVirtual.DelayedCall(.15f, () =>
         {
-            for (int j = -ExplosionSize.y; j <= ExplosionSize.y; j++)
+
+            for (int i = -ExplosionSize.x; i <= ExplosionSize.x; i++)
             {
-                if(i==0 && j ==0) continue;
-                var c = new int2(PositionOnBoard.x+i, PositionOnBoard.y +j);
-                if (_board.AreValidCoordinates(c))
+                for (int j = -ExplosionSize.y; j <= ExplosionSize.y; j++)
                 {
-                    if(_board[c]!=null)
-                        _board[c].HitByExplosion();
+                    if (i == 0 && j == 0) continue;
+                    var c = new int2(PositionOnBoard.x + i, PositionOnBoard.y + j);
+                    if (_board.AreValidCoordinates(c))
+                    {
+                        if (_board[c] != null)
+                            _board[c].HitByExplosion();
+                    }
                 }
             }
-        }
-        Despawn();
+            Despawn();
+        });
     }
 }
